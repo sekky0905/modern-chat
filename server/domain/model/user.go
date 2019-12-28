f646d3877d876b8599b398dc5f2eef3c0a9d3576
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-12-29 04:02:58 +0900

    add user domain model

---
+++ server/domain/model/user.go
@@ -0,0 +1,14 @@
+package model
+
+// UserID は、User の ID。
+type UserID string
+
+// String は、UserID を string 型にして返す。
+func (id UserID) String() string {
+	return string(id)
+}
+
+// User は、ユーザーを表す。
+type User struct {
+	ID UserID
+}
