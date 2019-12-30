16065db21faf8bcf1eca6aa8c4fcd77b74a53fe3
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-12-31 01:37:45 +0900

    fix repository's arg from gorm.DB to DB interface

--- server/domain/repository/comment.go
+++ server/domain/repository/comment.go
@@ -1,13 +1,12 @@
 package repository
 
 import (
-	"github.com/jinzhu/gorm"
 	"github.com/sekky0905/modern-chat/domain/model"
 )
 
 // CommentRepository は、Comment の Repository。
 type CommentRepository interface {
-	SaveComment(db *gorm.DB, room *model.Comment) (model.CommentID, error)
-	UpdateComment(db *gorm.DB, id model.CommentID, room *model.Comment) (model.CommentID, error)
-	DeleteComment(db *gorm.DB, id model.CommentID) (model.CommentID, error)
+	SaveComment(db DB, room *model.Comment) (model.CommentID, error)
+	UpdateComment(db DB, id model.CommentID, room *model.Comment) (model.CommentID, error)
+	DeleteComment(db DB, id model.CommentID) (model.CommentID, error)
 }
