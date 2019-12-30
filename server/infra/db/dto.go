27b046069dd456c23c66f86cacacf236f70e2580
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-12-30 23:46:43 +0900

    add DB model

---
+++ server/infra/db/dto.go
@@ -0,0 +1,79 @@
+package db
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/sekky0905/modern-chat/domain/model"
+)
+
+type AtFields struct {
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
+}
+
+// User は、ユーザーを表す。
+type User struct {
+	ID uint32 `gorm:"type:varchar(36)"`
+	AtFields
+}
+
+// TranslateToDomainModel は、domain model に移し替える。
+func (u *User) TranslateToDomainModel() *model.User {
+	return &model.User{
+		ID: model.UserID(u.ID),
+	}
+}
+
+// ChatRoom は、チャットルームを表す。
+type ChatRoom struct {
+	gorm.Model
+	Title  string `gorm:"type:varchar(20)"`
+	UserID string `gorm:"type:varchar(36)"`
+}
+
+// TranslateToDomainModel は、domain model に移し替える。
+func (c *ChatRoom) TranslateToDomainModel() *model.ChatRoom {
+	return &model.ChatRoom{
+		ID:        model.ChatRoomID(c.ID),
+		Title:     c.Title,
+		UserID:    model.UserID(c.UserID),
+		CreatedAt: c.CreatedAt,
+		UpdatedAt: c.UpdatedAt,
+	}
+}
+
+// Comment は、コメントを表す。
+type Comment struct {
+	gorm.Model
+	UserID     string `gorm:"type:varchar(36)"`
+	ChatRoomID uint32
+	Content    string `gorm:"type:varchar(200)"`
+}
+
+// TranslateToDomainModel は、domain model に移し替える。
+func (c *Comment) TranslateToDomainModel(likes []Like) *model.Comment {
+	n := len(likes)
+	s := make([]model.UserID, n, n)
+
+	for i, v := range likes {
+		s[i] = model.UserID(v.UserID)
+	}
+
+	return &model.Comment{
+		ID:         model.CommentID(c.ID),
+		UserID:     model.UserID(c.UserID),
+		ChatRoomID: model.ChatRoomID(c.ChatRoomID),
+		Liked:      s,
+		CreatedAt:  c.CreatedAt,
+		UpdatedAt:  c.UpdatedAt,
+	}
+}
+
+// Like は、いいねを表す。
+type Like struct {
+	UserID    uint32 `gorm:"type:varchar(36)"`
+	CommentID uint32
+	AtFields
+}
