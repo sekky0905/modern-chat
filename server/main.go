3d583aba2279e563a1503b108d9f4e2fdeff0d14
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-12-31 21:59:32 +0900

    fix

--- server/main.go
+++ server/main.go
@@ -1,24 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/sekky0905/modern-chat/infra/datastore"
-)
-
 func main() {
-	db, err := datastore.NewDB()
-	if err != nil {
-		s := fmt.Sprintf("new database error. err = %v", err)
-		panic(s)
-	}
-
-	defer func() {
-		if err := db.Close(); err != nil {
-			// TODO log は後に変更すること
-			log.Printf("close database error. err = %v\n", err)
-		}
-	}()
+	//db, err := datastore.NewDB()
+	//if err != nil {
+	//	s := fmt.Sprintf("new database error. err = %v", err)
+	//	panic(s)
+	//}
+	//
+	//defer func() {
+	//	if err := db.Close(); err != nil {
+	//		// TODO log は後に変更すること
+	//		log.Printf("close database error. err = %v\n", err)
+	//	}
+	//}()
 }
