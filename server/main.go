877bbc52f7317ae7c1147d3d717fa3899e318835
sekky0905 <ks.dev.sekky0905@gmail.com>
2019-12-31 22:37:42 +0900

    remove unnecessary commentlization

--- server/main.go
+++ server/main.go
@@ -1,16 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/sekky0905/modern-chat/server/infra/datastore"
+)
+
 func main() {
-	//db, err := datastore.NewDB()
-	//if err != nil {
-	//	s := fmt.Sprintf("new database error. err = %v", err)
-	//	panic(s)
-	//}
-	//
-	//defer func() {
-	//	if err := db.Close(); err != nil {
-	//		// TODO log は後に変更すること
-	//		log.Printf("close database error. err = %v\n", err)
-	//	}
-	//}()
+	db, err := datastore.NewDB()
+	if err != nil {
+		s := fmt.Sprintf("new database error. err = %v", err)
+		panic(s)
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			// TODO log は後に変更すること
+			log.Printf("close database error. err = %v\n", err)
+		}
+	}()
 }
