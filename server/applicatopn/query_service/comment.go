211a0a01be734732494e0b9b2e673b7b59877eab
sekky0905 <ks.dev.sekky0905@gmail.com>
2020-01-03 16:35:00 +0900

    add CommentQueryService

---
+++ server/applicatopn/query_service/comment.go
@@ -0,0 +1,8 @@
+package query_service
+
+import "github.com/sekky0905/modern-chat/server/applicatopn/dto"
+
+// CommentQueryService は、Comment 用の QueryService。
+type CommentQueryService interface {
+	ListComment() (*dto.CommentListDTO, error)
+}
