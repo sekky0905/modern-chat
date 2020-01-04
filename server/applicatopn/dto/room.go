216d3808e67500fab106832a95bf4f88d2f0f972
sekky0905 <ks.dev.sekky0905@gmail.com>
2020-01-05 00:33:52 +0900

    fix dto to be easy to handle from query service

--- server/applicatopn/dto/room.go
+++ server/applicatopn/dto/room.go
@@ -2,11 +2,26 @@ package dto
 
 import "time"
 
-// ChatRoomListDTO は、ChatRoom 用の List の STO.
-// TODO 後に無限スクロール系用の項目を追加するかも
+// ChatRoomListDTO は、ChatRoom 用の List の DTO
 type ChatRoomListDTO struct {
+	List []ChatRoomDTOForList
+	// TODO 後に無限スクロール系用の項目を追加するかも
+}
+
+// ChatRoomDTOForList は、ChatRoom の List 表示に用いる DTO.
+type ChatRoomDTOForList struct {
+	ChatRoomDTO
+	UserDTO
+}
+
+// ChatRoomDTO は、ChatRoom 用の DTO。
+type ChatRoomDTO struct {
 	ID        uint
 	Title     string
-	User      *UserDTO
 	CreatedAt time.Time
+}
+
+// TableName は、構造体に紐づける table の名前を返す。
+func (ChatRoomDTO) TableName() string {
+	return "chat_rooms"
 }
--- server/applicatopn/dto/user.go
+++ server/applicatopn/dto/user.go
@@ -2,6 +2,11 @@ package dto
 
 // UserDTO は、User 用の DTO。
 type UserDTO struct {
-	ID   string
-	Name string
+	UserID string `gorm:"column:id"`
+	Name   string
+}
+
+// TableName は、構造体に紐づける table の名前を返す。
+func (UserDTO) TableName() string {
+	return "users"
 }
